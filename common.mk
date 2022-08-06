71d3bea7d97f5efaba230605f3003028c50e4e91
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-08-06 23:49:32 +0300

    msm8998-common: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    https://github.com/PixelExp-Project/vendor_aosp/commit/c3b9f5c170c794966bb941282c3f9f18baa6279a
    
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -138,6 +138,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
