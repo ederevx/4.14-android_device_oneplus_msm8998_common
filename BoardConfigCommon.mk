74594f7ea4676d6421b405a874c755104658021a
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-03-31 22:21:27 +0200

    msm8998-common: update command line for kernel 4.14
    Take these from Sony (Yoshino):
    - coherent_pool: increase from the default (256kB),
    - lpm_levels.sleep_disabled: improve boot speed, and it's only used
      during boot (it's disabled after that).
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,8 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=16
+BOARD_KERNEL_CMDLINE += coherent_pool=8M
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += kpti=0
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
