235cd78774f19b6d278e5958a2d7615e6eb10d32
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-06-24 15:10:45 -0400

    msm8998-common: Include keys in vendor/lineage-priv
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- common.mk
+++ common.mk
@@ -258,6 +258,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Keys
+include vendor/lineage-priv/keys/keys.mk
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
