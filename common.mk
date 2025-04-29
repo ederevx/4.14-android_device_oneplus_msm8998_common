5dc5545d6596e05a9ffa98831bbde1c71aa3ce8c
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    msm8998-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -277,6 +277,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
